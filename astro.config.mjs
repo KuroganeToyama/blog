69adb838777470cf8c16af106330bb99be7b95b3
Di Nguyen <dingyen96@gmail.com>
2025-07-23 03:01:07 -0400

    Testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kuroganetoyama.github.io',
-  base: 'blog',
+  // base: 'blog',
   integrations: [mdx(), sitemap()],
   adapter: vercel(),
 });
