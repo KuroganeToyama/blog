55dba88380755e13497309310a67e0b0041500f2
Di Nguyen <dingyen96@gmail.com>
2025-07-23 02:39:00 -0400

    Testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,5 @@ export default defineConfig({
   site: 'https://kuroganetoyama.github.io',
   base: 'blog',
   integrations: [mdx(), sitemap()],
-  adapter: vercel(),
+  adapter: vercel({ imageService: true }),
 });
--- src/content/blog/2025-07-23-madoka-magica.md
+++ src/content/blog/2025-07-23-madoka-magica.md
@@ -2,7 +2,7 @@
 title: 'About Madoka Magica'
 description: 'About Madoka Magica'
 pubDate: 'July 23 2025'
-heroImage: '../../assets/blog-placeholder-1.jpg'
+heroImage: '../../assets/madoka-magica.jpg'
 ---
 
 Hello.
