1c692435628b3f75ee6fe9a2042b0b4b07ad95ed
Di Nguyen <dingyen96@gmail.com>
2025-07-23 00:43:14 -0400

    Updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import fs from 'fs';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://kuroganetoyama.github.io',
+	base: 'blog',
 	integrations: [mdx(), sitemap()],
-	vite: {
-		build: {
-			rollupOptions: {
-				plugins: [
-					{
-						name: 'add-nojekyll',
-						writeBundle() {
-							fs.writeFileSync('./dist/.nojekyll', '');
-						}
-					}
-				]
-			}
-		}
-	}
 });
