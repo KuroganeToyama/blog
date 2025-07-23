49e82e69bb126190b34612dbb8016daa9b5a423a
Di Nguyen <dingyen96@gmail.com>
2025-07-23 00:40:02 -0400

    Added no jekyll rule

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,24 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import fs from 'fs';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://kuroganetoyama.github.io',
 	integrations: [mdx(), sitemap()],
+	vite: {
+		build: {
+			rollupOptions: {
+				plugins: [
+					{
+						name: 'add-nojekyll',
+						writeBundle() {
+							fs.writeFileSync('./dist/.nojekyll', '');
+						}
+					}
+				]
+			}
+		}
+	}
 });
