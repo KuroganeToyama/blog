0301dad2a16b3e2b5b6d2b4ba2c2954b8d27d79e
Di Nguyen <dingyen96@gmail.com>
2025-07-23 00:50:30 -0400

    Added Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://kuroganetoyama.github.io',
-	base: 'blog',
-	integrations: [mdx(), sitemap()],
+  site: 'https://kuroganetoyama.github.io',
+  base: 'blog',
+  integrations: [mdx(), sitemap()],
+  adapter: vercel(),
 });
